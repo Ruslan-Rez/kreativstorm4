7835abc56639953767bb203b9ded0bdf681dd52e
Ashish Augustine <ashish.augustine123@gmail.com>
2023-08-08 01:07:19 +0200

    commit

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 let displayValue;
+function change_send(value) {
+  //keyboard
+  document.getElementById("display").value = value;
+  displayValue = document.getElementById("display").value;
+  console.log("Current display value" + displayValue);
+}
 
 function appendToDisplay(value) {
+  //mouse
   document.getElementById("display").value += value;
   displayValue = document.getElementById("display").value;
-  console.log("The display value is " + displayValue);
+  console.log("Current display value" + displayValue);
 }
 
 function clearDisplay() {
   document.getElementById("display").value = "";
 }
 
-function add(number1, number2){
+function add(number1, number2) {
   let add = number1 + number2;
   return add;
 }
-function subtract(number1, number2){
+function subtract(number1, number2) {
   let sub = number1 - number2;
   return sub;
 }
-function multiply(number1,number2){
+function multiply(number1, number2) {
   let mul = number1 * number2;
-  return mul
+  return mul;
 }
-function divide(number1, number2){
+function divide(number1, number2) {
   let div = number1 / number2;
-  return div
+  return div;
 }
-function operate(number1, number2, operator){
-  switch (operator){
+function operate(number1, number2, operator) {
+  switch (operator) {
     case "+":
-      return add(number1,number2)
+      return add(number1, number2);
     case "-":
-      return subtract(number1,number2)
+      return subtract(number1, number2);
     case "*":
-      return multiply(number1,number2)
+      return multiply(number1, number2);
     case "/":
-      if (number2 === 0){
-        return "Why..."
-      }else return divide(number1,number2)
+      if (number2 === 0) {
+        return "Why...";
+      } else return divide(number1, number2);
   }
 }
