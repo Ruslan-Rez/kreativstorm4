930bd02faa5e3dd6c97e3e4f9f9c392d9416e0ca
trudyjograce <jo.1981@yahoo.com>
2023-08-07 20:00:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 function appendToDisplay(value) {
   document.getElementById("display").value += value;
+  displayValue = document.getElementById("display").value;
+  console.log("The display value is " + displayValue);
 }
+
 
 function clearDisplay() {
   document.getElementById("display").value = "";
 }
-
-function add(number1, number2){
+function divide(number1, number2) {
+  let div = number1 / number2;
+  return div;
+}
+function add(number1, number2) {
   let add = number1 + number2;
   return add;
 }
-function subtract(number1, number2){
+function subtract(number1, number2) {
   let sub = number1 - number2;
   return sub;
 }
-function multiply(number1,number2){
+function multiply(number1, number2) {
   let mul = number1 * number2;
-  return mul
+  return mul;
 }
-function divide(number1, number2){
-  let div = number1 / number2;
-  return div
-}
-function operate(number1, number2, operator){
-  switch (operator){
-    case "+":
-      return add(number1,number2)
-    case "-":
-      return subtract(number1,number2)
-    case "*":
-      return multiply(number1,number2)
+
+
+function operate(number1, number2, operator) {
+  switch (operator) {
     case "/":
-      if (number2 === 0){
-        return "Why..."
-      }else return divide(number1,number2)
+      if (number2 === 0) {
+        return "Why...";
+      } else return divide(number1, number2);
+      case "*":
+      return multiply(number1, number2);
+    case "+":
+      return add(number1, number2);
+    case "-":
+      return subtract(number1, number2);
   }
 }
+
