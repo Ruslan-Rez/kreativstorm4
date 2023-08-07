5135f136d61339348f8056e936e023069ff975c2
Ruslan Rezunenko <rusy-@ukr.net>
2023-08-07 15:04:26 +0200

    did part a) and b), updated the style of the calculator

--- script.js
+++ script.js
@@ -6,11 +6,33 @@ function clearDisplay() {
   document.getElementById("display").value = "";
 }
 
-function calculate() {
-  const display = document.getElementById("display");
-  try {
-    display.value = eval(display.value);
-  } catch (error) {
-    display.value = "Error";
+function add(number1, number2){
+  let add = number1 + number2;
+  return add;
+}
+function subtract(number1, number2){
+  let sub = number1 - number2;
+  return sub;
+}
+function multiply(number1,number2){
+  let mul = number1 * number2;
+  return mul
+}
+function divide(number1, number2){
+  let div = number1 / number2;
+  return div
+}
+function operate(number1, number2, operator){
+  switch (operator){
+    case "+":
+      return add(number1,number2)
+    case "-":
+      return subtract(number1,number2)
+    case "*":
+      return multiply(number1,number2)
+    case "/":
+      if (number2 === 0){
+        return "Why..."
+      }else return divide(number1,number2)
   }
 }
