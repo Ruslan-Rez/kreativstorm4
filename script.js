8dca532e3969426fa768e6d04a5ba03b166d164f
Ashish Augustine <ashish.augustine123@gmail.com>
2023-08-09 09:54:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,4 @@
-
-const input = document.querySelector("#display");
-const numbers = document.querySelectorAll(".number");
-const operators = document.querySelectorAll(".operand");
-const equal = document.querySelector(".equals");
-const clear = document.querySelector(".clear");
 let displayValue;
-
-let number1  = " ";
-let number2 = " ";
-let operator =" ";
-
-// listen for numbers
-numbers.forEach(number => {
-  number.addEventListener("click", e => {
-      if (operator === " ") { 
-        // Read first number if no operator set yet
-        number1 = parseInt(e.target.value);
-      } else { 
-        // Read second number
-          number2 = parseInt(e.target.value);
-      }
-  });
-});
-
-// listen for operators
-operators.forEach(operatorInput => {
-  operatorInput.addEventListener("click", e => {
-    // If the operator is not equals
-      if (e.target.value !== "=") { 
-          operator = e.target.value;
-          console.log("number1 from op:" +number1); 
-          console.log("operator" + operator); 
-      } else { 
-          console.log(number2); 
-          // If equals button clicked
-          operate()
-      }})})
-  
-
-let displayValue;
-
 function change_send(value) {
   //keyboard
   document.getElementById("display").value = value;
@@ -51,18 +10,16 @@ function appendToDisplay(value) {
   //mouse
   document.getElementById("display").value += value;
   displayValue = document.getElementById("display").value;
-
-  console.log("Current display value" + displayValue)
-  console.log("Number1 is:" +number1)
-
   console.log("Current display value" + displayValue);
-
 }
 
-
-// clear.addEventListener("click", clearDisplay)
 function clearDisplay() {
   document.getElementById("display").value = "";
+}
+function backSpace() {
+  displayValue = displayValue.substring(0, displayValue.length - 1);
+  document.getElementById("display").value = displayValue;
+  console.log("Current display value" + displayValue);
 }
 
 function add(number1, number2) {
@@ -81,9 +38,6 @@ function divide(number1, number2) {
   let div = number1 / number2;
   return div;
 }
-
-
-
 function operate(number1, number2, operator) {
   switch (operator) {
     case "+":
@@ -96,6 +50,5 @@ function operate(number1, number2, operator) {
       if (number2 === 0) {
         return "Why...";
       } else return divide(number1, number2);
-
   }
 }
