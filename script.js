d5e5ad288be424c895798e14f7d4676a7979e5c2
Amr Mohamed <arsmohamed@outlook.com>
2023-08-10 10:03:07 -0230

    add the calculate and testing it with the dot

--- script.js
+++ script.js
@@ -2,61 +2,80 @@ const display = document.querySelector("#display");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operand");
 const clear = document.querySelector(".clear");
+const equal = document.querySelector("#equal");
 
-let displayValue = null;
+let displayValue = "";
 let number1 = null;
 let number2 = null;
 let operator = null;
+let hasCalculated = false;
+
+numbers.forEach((numberInput) => {
+  numberInput.addEventListener("click", (e) => {
+    appendToDisplay(e.target.value);
+  });
+});
 
 // listen for operators being applied
 operators.forEach((operatorInput) => {
   operatorInput.addEventListener("click", (e) => {
-
     // If the operator is not equals
     if (e.target.value !== "=") {
-      number1 = parseFloat(displayValue)
+      number1 = parseFloat(displayValue);
       console.log("Number1 is:" + number1);
-      
+
       // operator is operator pressed
       operator = e.target.value;
       console.log("OPerator value is:" + operator);
 
       // clear display
       display.value = "";
-      displayValue = display.value
+      displayValue = display.value;
     } else {
-      displayValue = display.value
-      number2 = parseFloat(displayValue)
-      console.log("num2:" + number2);  
+      displayValue = display.value;
+      number2 = parseFloat(displayValue);
+      console.log("num2:" + number2);
     }
   });
 });
 
-document.getElementById("equal").addEventListener('click', calculate)
-function calculate(){
-  let solution = operate(number1,number2, operator);
-  console.log("Current display value" + solution);
-  display.value = solution;
+// Listen for equal button click
+function calculate() {
+  if (number1 !== null && operator !== null) {
+    number2 = parseFloat(displayValue);
+    const result = operate(number1, number2, operator);
+    display.value = result;
+    number1 = result;
+    number2 = null;
+    operator = null;
+    displayValue = "";
+  }
 }
 
 function change_send(value) {
   //keyboard
-  document.getElementById("display").value = value;
-  displayValue = document.getElementById("display").value;
-  console.log("Current display value" + displayValue);
+  display.value = value;
+  displayValue = value;
+  // document.getElementById("display").value = value;
+  // displayValue = document.getElementById("display").value;
+  console.log("Current display value: " + displayValue);
 }
 
 function appendToDisplay(value) {
   //mouse
-  document.getElementById("display").value += value;
-  displayValue = document.getElementById("display").value;
-  console.log("Current display value" + displayValue);
+  display.value += value;
+  displayValue = display.value;
+  // document.getElementById("display").value += value;
+  // displayValue = document.getElementById("display").value;
+  console.log("Current display value: " + displayValue);
 }
 
 // clear.addEventListener("click", clearDisplay)
 function clearDisplay() {
-  document.getElementById("display").value = "";
-  displayValue = null;
+  // document.getElementById("display").value = "";
+  display.value = "";
+  displayValue = "";
+  // displayValue = null;
   number1 = null;
   number2 = null;
   operator = null;
@@ -66,7 +85,7 @@ function backSpace() {
   if (displayValue) {
     displayValue = displayValue.substring(0, displayValue.length - 1);
     document.getElementById("display").value = displayValue;
-    console.log("Current display value" + displayValue);
+    console.log("Current display value: " + displayValue);
   }
 }
 
@@ -89,15 +108,15 @@ function divide(number1, number2) {
 
 function operate(number1, number2, operator) {
   switch (operator) {
-    case '+':
+    case "+":
       return add(number1, number2);
-    case '-':
+    case "-":
       return subtract(number1, number2);
-    case '*':
+    case "*":
       return multiply(number1, number2);
-    case '/':
+    case "/":
       if (number2 === 0) {
-        return "Why...";
+        return "Why ...";
       } else return divide(number1, number2);
   }
 }
